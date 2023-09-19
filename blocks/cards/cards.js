1b80d0f6f4c7ccadb906e4cc57be379b8a3d1ef1
Laura van Os <88458945+Lauraa1003@users.noreply.github.com>
2023-09-19 10:36:30 +0200

    Created blog cards (#9)

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,18 +1,64 @@
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
-export default function decorate(block) {
-  /* change to ul, li */
+function generateBlogCard(blogData) {
+  const keywordsArray = blogData.keywords ? blogData.keywords.split(',').map((keyword) => keyword.trim()) : [];
+
+  return `
+        <li class="blog-card">
+            <div class="blog-card-image">
+                ${createOptimizedPicture(blogData.image, blogData.imageAlt || blogData.title, false, [{ width: '750' }]).outerHTML}
+            </div>
+            <div class="blog-card-content">
+                <div class="blog-card-title">
+                    <h3>${blogData.title}</h3>
+                </div>
+                <div class="blog-card-description">
+                    <p>${blogData.description}</p>
+                    <div class="blog-card-author">
+                        Written by: <span>${blogData.author}</span>
+                    </div>
+                </div>
+                ${keywordsArray.length ? `<ul class="blog-card-tags">${keywordsArray.map((keyword) => `<li>${keyword}</li>`).join('')}</ul>` : ''}
+            </div>
+            <a href="${blogData.path}" class="blog-card-link"">Continue Reading</a>
+        </li>
+    `;
+}
+
+export default async function decorate(block) {
   const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+
+  if (block.classList.contains('blog')) {
+    let blogData = [];
+
+    try {
+      const req = await fetch('/query-index.json');
+      if (req.ok) {
+        const res = await req.json();
+        blogData = res.data;
+      } else {
+        return;
+      }
+    } catch (err) {
+      return;
+    }
+
+    blogData.forEach((data) => {
+      const li = generateBlogCard(data);
+      ul.innerHTML += li;
     });
-    ul.append(li);
-  });
-  ul.querySelectorAll('picture img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  } else {
+    [...block.children].forEach((row) => {
+      const li = document.createElement('li');
+      while (row.firstElementChild) li.append(row.firstElementChild);
+      [...li.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+        else div.className = 'cards-card-body';
+      });
+      ul.append(li);
+    });
+  }
+
   block.textContent = '';
   block.append(ul);
 }
