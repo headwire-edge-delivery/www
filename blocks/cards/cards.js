d0c3647d67c8b921d2ac71facb8b6cc6f1503a69
laura~ <lauraavanos@hotmail.com>
2023-09-19 08:21:05 +0200

    linting

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,9 +1,9 @@
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
 function generateBlogCard(blogData) {
-    const keywordsArray = blogData.keywords ? blogData.keywords.split(',').map(keyword => keyword.trim()) : [];
+  const keywordsArray = blogData.keywords ? blogData.keywords.split(',').map((keyword) => keyword.trim()) : [];
 
-    return `
+  return `
         <li class="blog-card">
             <div class="blog-card-image">
                 ${createOptimizedPicture(blogData.image, blogData.imageAlt || blogData.title, false, [{ width: '750' }]).outerHTML}
@@ -18,51 +18,48 @@ function generateBlogCard(blogData) {
                         Written by: <span>${blogData.author}</span>
                     </div>
                 </div>
-                ${keywordsArray.length ? `<ul class="blog-card-tags">${keywordsArray.map(keyword => `<li>${keyword}</li>`).join('')}</ul>` : ''}
+                ${keywordsArray.length ? `<ul class="blog-card-tags">${keywordsArray.map((keyword) => `<li>${keyword}</li>`).join('')}</ul>` : ''}
             </div>
             <a href="${blogData.path}" class="blog-card-link">Read more</a>
         </li>
     `;
 }
 
-
-
 export default async function decorate(block) {
-    const ul = document.createElement('ul');
-    
-    if (block.classList.contains('blog')) {
-        let blogData = [];
+  const ul = document.createElement('ul');
 
-        try {
-            const req = await fetch('/query-index.json');
-            if (req.ok) {
-                const res = await req.json();
-                blogData = res.data;
-                console.log(blogData)
-            } else {
-                console.error("Failed to fetch data");
-            }
-        } catch (err) {
-            console.error("Error fetching data:", err);
-        }
+  if (block.classList.contains('blog')) {
+    let blogData = [];
 
-        blogData.forEach(data => {
-            const li = generateBlogCard(data);
-            ul.innerHTML += li;
-        });
-    } else {
-        [...block.children].forEach((row) => {
-            const li = document.createElement('li');
-            while (row.firstElementChild) li.append(row.firstElementChild);
-            [...li.children].forEach((div) => {
-                if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-                else div.className = 'cards-card-body';
-            });
-            ul.append(li);
-        });
-        ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+    try {
+      const req = await fetch('/query-index.json');
+      if (req.ok) {
+        const res = await req.json();
+        blogData = res.data;
+      } else {
+        return;
+      }
+    } catch (err) {
+      return;
     }
 
-    block.textContent = '';
-    block.append(ul);
+    blogData.forEach((data) => {
+      const li = generateBlogCard(data);
+      ul.innerHTML += li;
+    });
+  } else {
+    [...block.children].forEach((row) => {
+      const li = document.createElement('li');
+      while (row.firstElementChild) li.append(row.firstElementChild);
+      [...li.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+        else div.className = 'cards-card-body';
+      });
+      ul.append(li);
+    });
+    ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  }
+
+  block.textContent = '';
+  block.append(ul);
 }
