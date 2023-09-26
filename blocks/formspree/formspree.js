01363a2aa92ebe40a6b13d8365346dc90d57d4f4
icaraps <icaraps@hotmail.com>
2023-09-26 11:24:42 +0400

    minor

--- blocks/formspree/formspree.js
+++ blocks/formspree/formspree.js
@@ -16,7 +16,7 @@ export default async function decorate(block) {
         <label for="email-address">Email Address</label>
         <input type="email" name="_replyto" id="email-address" placeholder="email@domain.tld" required="">
         <label for="message">Message</label>
-        <textarea rows="5" name="message" id="message" placeholder="Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla nullam quis risus." required=""></textarea>
+        <textarea rows="5" name="message" id="message" placeholder="Your message here." required=""></textarea>
         <input type="hidden" name="_subject" id="email-subject" value="Contact Form Submission">
       </fieldset>
       <input type="submit" value="Submit">
