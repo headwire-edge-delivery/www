fd73cb8d543f220a655a5bf0da3d2d9fde4c8ab8
laura~ <lauraavanos@hotmail.com>
2023-10-05 12:14:48 +0200

    cookie delayed test

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,5 @@ import { sampleRUM } from './lib-franklin.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+<script src="https://app.enzuzo.com/apps/enzuzo/static/js/__enzuzo-cookiebar.js?uuid=04cbcc9c-62ba-11ee-b344-331207534791"></script>
