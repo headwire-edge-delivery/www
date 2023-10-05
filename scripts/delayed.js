3a776a24da563103f22efc18cca3384f326cd92b
laura~ <lauraavanos@hotmail.com>
2023-10-05 12:24:43 +0200

    cookie test

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,5 +5,16 @@ import { sampleRUM } from './lib-franklin.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
-
-<script src="https://app.enzuzo.com/apps/enzuzo/static/js/__enzuzo-cookiebar.js?uuid=04cbcc9c-62ba-11ee-b344-331207534791"></script>
+if (window.location.hostname === 'www.headwire.workers.dev') {
+  document.head.insertAdjacentHTML('beforeend', `
+    <script src="https://app.enzuzo.com/apps/enzuzo/static/js/__enzuzo-cookiebar.js?uuid=04cbcc9c-62ba-11ee-b344-331207534791"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-C3VLD9MKXK"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+    
+        gtag('config', 'G-C3VLD9MKXK');
+    </script>
+  `);
+}
