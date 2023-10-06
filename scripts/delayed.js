64ee6be8bf9d26973d6d93c3c85ecd5a75743de4
laura~ <lauraavanos@hotmail.com>
2023-10-06 09:05:49 +0200

    move analytics to delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,17 @@ import { sampleRUM } from './lib-franklin.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+// Analytics
+if (window.location.hostname === 'www.headwire.workers.dev') {
+    document.head.insertAdjacentHTML('beforeend', `
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-C3VLD9MKXK"></script>
+      <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+      
+          gtag('config', 'G-C3VLD9MKXK');
+      </script>
+    `);
+  }
