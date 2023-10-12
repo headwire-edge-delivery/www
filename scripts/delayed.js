66f2e3ccb85fd87ea8755eb423b90968cb28e774
laura~ <lauraavanos@hotmail.com>
2023-10-12 14:58:13 +0200

    analytics test

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -7,15 +7,15 @@ sampleRUM('cwv');
 // add more delayed functionality here
 
 // Analytics
-if (window.location.hostname === 'www.headwire.workers.dev') {
-  document.head.insertAdjacentHTML('beforeend', `
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-C3VLD9MKXK"></script>
-      <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+// if (window.location.hostname === 'www.headwire.workers.dev') {
+//   document.head.insertAdjacentHTML('beforeend', `
+//       <script async src="https://www.googletagmanager.com/gtag/js?id=G-C3VLD9MKXK"></script>
+//       <script>
+//           window.dataLayer = window.dataLayer || [];
+//           function gtag(){dataLayer.push(arguments);}
+//           gtag('js', new Date());
       
-          gtag('config', 'G-C3VLD9MKXK');
-      </script>
-    `);
-}
+//           gtag('config', 'G-C3VLD9MKXK');
+//       </script>
+//     `);
+// }
